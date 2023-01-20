4c7501fd208fb8818d8dbde499ab1a44e98603a0
AnsonCHOWcm <74531677+AnsonCHOWcm@users.noreply.github.com>
2023-01-20 23:59:53 +0800

    commit changes

--- index_chrome_ext.js
+++ index_chrome_ext.js
@@ -1,3 +1,27 @@
-let inputBtn = document.getElementById("input-btn")
+let myLead = []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEL = document.getElementById("ul-el")
 
-inputBtn.addEventListener("click", function(){console.log("button is clicked")})
+inputBtn.addEventListener("click", function() {
+    myLead.push(inputEl.value)
+    render()
+    inputEl.value = ""
+})
+
+//create a variable, listItems, to hold all HTML
+
+function render (){
+
+    let listItems = ""
+
+for (let i = 0; i < myLead.length; i++){
+
+    listItems += "<li><a target = '_blank' href='" +  myLead[i]  + "'>" + myLead[i] + "</a></li>"
+
+}
+
+ulEL.innerHTML = listItems
+
+}
+
