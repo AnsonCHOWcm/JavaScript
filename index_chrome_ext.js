9deca2f175236019150e1e6b5708c154ca1e6b29
AnsonCHOWcm <74531677+AnsonCHOWcm@users.noreply.github.com>
2023-01-21 01:45:36 +0800

    commits changes

--- index_chrome_ext.js
+++ index_chrome_ext.js
@@ -1,4 +1,9 @@
 let myLead = []
+if (JSON.parse(localStorage.getItem("myLead"))){
+    myLead = JSON.parse(localStorage.getItem("myLead"))
+} else {
+    myLead = []
+}
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEL = document.getElementById("ul-el")
@@ -7,6 +12,7 @@ inputBtn.addEventListener("click", function() {
     myLead.push(inputEl.value)
     render()
     inputEl.value = ""
+    localStorage.setItem("myLead", JSON.stringify(myLead))
 })
 
 //create a variable, listItems, to hold all HTML
@@ -17,11 +23,19 @@ function render (){
 
 for (let i = 0; i < myLead.length; i++){
 
-    listItems += "<li><a target = '_blank' href='" +  myLead[i]  + "'>" + myLead[i] + "</a></li>"
-
+    //listItems += "<li><a target = '_blank' href='" +  myLead[i]  + "'>" + myLead[i] + "</a></li>"
+    listItems += `
+                 <li>
+                     <a target = '_blank' href = '${myLead[i]}'>
+                     ${myLead[i]}
+                     </a>
+                </li>
+    `
 }
 
 ulEL.innerHTML = listItems
 
 }
+
+
 
