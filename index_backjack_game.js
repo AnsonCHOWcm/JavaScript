1069e514f18a63160649ac10ad8b8d091ae3cbeb
AnsonCHOWcm <74531677+AnsonCHOWcm@users.noreply.github.com>
2023-01-20 12:12:08 +0800

    Created HTML, CSS and JS for Project 3 : Google Extension

--- index_backjack_game.js
+++ index_backjack_game.js
@@ -1,24 +1,41 @@
 // create two variable, firstCard and secondCard
 // Set their values to a random number between 2-11
 
-let = firstCard = 0
+let firstCard = 0
 let secondCard = 0
+let cards = []
 let sum = 0
 let hasBlackjack = false
-let isalive = true
+let isalive = false
 let message = ""
 let game_mess_El = document.getElementById("game-message-el")
 let card_El = document.getElementById("card-el")
 let res_El = document.getElementById("result-el")
+let playerEl = document.getElementById("player-el")
+
+let player = {
+    Name: "Anson",
+    Chips: 145,
+    sayHello: function() {
+        console.log("Hello")
+    },
+
+}
+
+playerEl.textContent = player.Name + ": $ " + player.Chips
 
 // Set a variable, sum, and set it to be the sum of firstcard and secondcard
 
 function startgame(){
 
-    firstCard = Math.floor(Math.random() * 10 +2)
-    secondCard = Math.floor(Math.random() * 10 +2)
+    cards = []
 
-    sum = firstCard + secondCard
+    cards.push(getrandomcard())
+    cards.push(getrandomcard())
+
+    sum = cards[0] + cards[1]
+
+    isalive = true
 
     if (sum <= 20){
         message = "Do you want to draw a new card"
@@ -30,7 +47,7 @@ function startgame(){
         isalive = false
     }
 
-    card_El.textContent = "Cards : "+firstCard + ", " + secondCard
+    card_El.textContent = "Cards : "+ cards[0] + " " + cards[1]
     res_El.textContent = "Result : " + sum
 
     game_mess_El.textContent = message
@@ -38,7 +55,13 @@ function startgame(){
 }
 
 function drawanothercard(){
-    let new_card = Math.floor(Math.random() * 10 +2)
+
+    if (isalive === false || hasBlackjack === true) {
+        return
+    }
+
+    new_card = getrandomcard()
+    cards.push(new_card)
 
     sum += new_card
 
@@ -52,13 +75,27 @@ function drawanothercard(){
         isalive = false
     }
 
+    card_El.textContent = "Card : "
 
-    card_El.textContent += ", " + new_card
+    for (let i = 0; i < cards.length; i++){
+        card_El.textContent += cards[i] + " "
+    }
     res_El.textContent = "Result : " + sum
 
     game_mess_El.textContent = message
 
 
+}
+
+function getrandomcard(){
+    let res = Math.floor(Math.random() * 13 +1)
+    if (res === 1){
+        res = 11
+    }else if (res > 10){
+        res = 10
+    }
+
+    return res
 }
 
 console.log(message)
