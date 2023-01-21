8df78f4e65407493b488c044573db398310031e4
AnsonCHOWcm <74531677+AnsonCHOWcm@users.noreply.github.com>
2023-01-21 15:53:11 +0800

    finished final versionof the google extension

--- index_chrome_ext.js
+++ index_chrome_ext.js
@@ -1,5 +1,5 @@
 let myLead = []
-let LeadsFromLocalStorage = localStorage.getItem("myLead")
+const LeadsFromLocalStorage = localStorage.getItem("myLead")
 if (JSON.parse(LeadsFromLocalStorage)){
     myLead = JSON.parse(LeadsFromLocalStorage)
 } else {
@@ -10,32 +10,46 @@ const inputBtn = document.getElementById("input-btn")
 const delBtn = document.getElementById("del-btn")
 const ulEL = document.getElementById("ul-el")
 
+const tabBtn = document.getElementById("tab-btn")
+
 inputBtn.addEventListener("click", function() {
     myLead.push(inputEl.value)
-    render()
+    render(myLead)
     inputEl.value = ""
     localStorage.setItem("myLead", JSON.stringify(myLead))
 })
 
-delBtn.addEventListener("click", function() {
-    ClearLocalLeads()
+tabBtn.addEventListener("click", function(){
+    //Grab the current URL 
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        myLead.push(tabs[0].url)
+        render(myLead)
+        localStorage.setItem("myLead", JSON.stringify(myLead))
+    })
+
 })
 
-render()
+delBtn.addEventListener("dblclick", function() {
+    ClearLocalLeads()
+    myLead = []
+    render(myLead)
+})
+
+render(myLead)
 
 //create a variable, listItems, to hold all HTML
 
-function render (){
+function render (leads){
 
     let listItems = ""
 
-for (let i = 0; i < myLead.length; i++){
+for (let i = 0; i < leads.length; i++){
 
     //listItems += "<li><a target = '_blank' href='" +  myLead[i]  + "'>" + myLead[i] + "</a></li>"
     listItems += `
                  <li>
-                     <a target = '_blank' href = '${myLead[i]}'>
-                     ${myLead[i]}
+                     <a target = '_blank' href = '${leads[i]}'>
+                     ${leads[i]}
                      </a>
                 </li>
     `
@@ -47,8 +61,6 @@ ulEL.innerHTML = listItems
 
 function ClearLocalLeads(){
     localStorage.clear()
-    myLead = []
-    render()
 }
 
 
